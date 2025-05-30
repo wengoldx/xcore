516d0a0ef800027e6ba429fe18f2c4aa80574fd3
youhei <ping.yang@wengold.net>
2025-05-30 15:35:02 +0800

    Add upgrade milestone version datas

--- nacos/constants.go
+++ nacos/constants.go
@@ -94,12 +94,21 @@ type MallAcc struct {
 
 // Nacos config for OTA upgrade by using DID_OTA_BUILDS data id
 type OTAInfo struct {
-	BuildVersion string  `json:"BuildVersion" description:"Build version string"`
-	BuildNumber  int     `json:"BuildNumber"  description:"Build number, pase form BuildVersion string as version = major*10000 + middle*100 + minor"`
-	DownloadUrl  string  `json:"DownloadUrl"  description:"Bin file download url"`
-	UpdateDate   string  `json:"UpdateDate"   description:"Bin file update date"`
-	HashSums     string  `json:"HashSums"     description:"Bin file hash sums"`
-	BinSizes     float64 `json:"BinSizes"     description:"Bin file sizes in MB"`
+	BuildVersion string       `json:"BuildVersion"        description:"Build version string"`
+	BuildNumber  int          `json:"BuildNumber"         description:"Build number, pase form BuildVersion string as version = major*10000 + middle*100 + minor"`
+	DownloadUrl  string       `json:"DownloadUrl"         description:"Bin file download url"`
+	UpdateDate   string       `json:"UpdateDate"          description:"Bin file update date"`
+	HashSums     string       `json:"HashSums"            description:"Bin file hash sums"`
+	BinSizes     float64      `json:"BinSizes"            description:"Bin file sizes in MB"`
+	Milestone    []*Milestone `json:"Milestone,omitempty" description:"Bin file sizes in MB"`
+}
+
+// Milestone version for upgrade as the important node in sequence.
+type Milestone struct {
+	BuildNumber int     `json:"BuildNumber" description:"Build number, pase form BuildVersion string as version = major*10000 + middle*100 + minor"`
+	DownloadUrl string  `json:"DownloadUrl" description:"Bin file download url"`
+	HashSums    string  `json:"HashSums"    description:"Bin file hash sums"`
+	BinSizes    float64 `json:"BinSizes"    description:"Bin file sizes in MB"`
 }
 
 // Nacos config for DingTalk notify sender
