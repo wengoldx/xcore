4a197598cf680a8600ac1aff0bbd37678b6ab7b3
youhei <ping.yang@wengold.net>
2025-12-12 10:18:36 +0800

    update

--- utils/wing_timecount.go
+++ utils/wing_timecount.go
@@ -20,51 +20,66 @@ import (
 // Time counter for calculate used durations.
 type timeCounter struct {
 	start int64 // Start time in unix nano second.
-	used  int64 // Used durations of unix nano second.
+	tick  int64 // Tick start time for count tick interval.
 }
 
 // Create a new time counter for calculate duration and logs.
 func NewTimeCounter() timeCounter {
-	return timeCounter{
-		start: time.Now().UnixNano(),
-	}
+	return timeCounter{start: time.Now().UnixNano()}
 }
 
 // Count the used duration time after counter create or called Reset().
 func (c *timeCounter) Count() int64 {
-	c.used = time.Now().UnixNano() - c.start
-	return c.used
+	used := time.Now().UnixNano() - c.start
+	return used
+}
+
+// Count the tick interval after counter create or called Reset().
+func (c *timeCounter) Tick() int64 {
+	c.tick = time.Now().UnixNano() - c.tick
+	return c.tick
 }
 
 // Reset start time and clear used duration value.
 func (c *timeCounter) Reset() {
-	c.start, c.used = time.Now().UnixNano(), 0
+	c.start = time.Now().UnixNano()
+	c.tick = c.start
 }
 
 // Count and logout the used duration on auto calculate unit.
-func (c *timeCounter) LogUsed(msg string, custom ...bool) {
+func (c *timeCounter) LogUsed(msg string, islogger ...bool) {
 	used := c.Count()
-	if len(custom) > 0 && custom[0] {
+	if Variable(islogger, false) {
 		logger.If("%s: %s", msg, formatDuration(used))
-	} else {
-		fmt.Printf("%s: %s\n", msg, formatDuration(used))
+		return
 	}
+	fmt.Printf("%s: %s\n", msg, formatDuration(used))
+}
+
+// Count and logout the used duration on auto calculate unit.
+func (c *timeCounter) LogTick(msg string, islogger ...bool) {
+	interval := c.Tick()
+	if Variable(islogger, false) {
+		logger.If("%s: %s", msg, formatDuration(interval))
+		return
+	}
+	fmt.Printf("%s: %s\n", msg, formatDuration(interval))
 }
 
 const (
-	_second      = int64(time.Second)      // 1000 x 1000 x 1000 ns
-	_millisecond = int64(time.Millisecond) // 1000 x 1000 ns
-	_microsecond = int64(time.Microsecond) // 1000 ns
+	_tc_s  = int64(time.Second)      // 1000 x 1000 x 1000 ns
+	_tc_ms = int64(time.Millisecond) // 1000 x 1000 ns
+	_tc_us = int64(time.Microsecond) // 1000 ns
 )
 
 // Format used time like: 1.234s, 56.789ms, 123.456us, 234ns.
 func formatDuration(used int64) string {
-	if used > _second {
-		return fmt.Sprintf("%v.%v s", used/_second, (used%_second)/_millisecond)
-	} else if used > _millisecond {
-		return fmt.Sprintf("%v.%v ms", used/_millisecond, (used%_millisecond)/_microsecond)
-	} else if used > _microsecond {
-		return fmt.Sprintf("%v.%v us", used/_microsecond, used%_microsecond)
+	if used > _tc_s {
+		return fmt.Sprintf("%v.%v s", used/_tc_s, (used%_tc_s)/_tc_ms)
+	} else if used > _tc_ms {
+		return fmt.Sprintf("%v.%v ms", used/_tc_ms, (used%_tc_ms)/_tc_us)
+	} else if used > _tc_us {
+		return fmt.Sprintf("%v.%v us", used/_tc_us, used%_tc_us)
 	} else {
 		return fmt.Sprintf("%v ns", used)
 	}
