163a4f545c6a88e44d2f835cfc1a747c99f70110
youhei <ping.yang@wengold.net>
2026-03-17 13:38:47 +0800

    update

---
+++ secure/crypto_aes_test.go
@@ -0,0 +1,26 @@
+// Copyright (c) 2018-Now Dunyu All Rights Reserved.
+//
+// Author      : https://www.wengold.net
+// Email       : support@wengold.net
+//
+// Prismy.No | Date       | Modified by. | Description
+// -------------------------------------------------------------------
+// 00001       2019/05/22   yangping       New version
+// -------------------------------------------------------------------
+
+package secure
+
+import (
+	"fmt"
+	"testing"
+)
+
+// -------------------------------------------------------------------
+// USAGE: Enter ~/xcore/secure, and excute command to test.
+//
+//	go test -v -cover
+// -------------------------------------------------------------------
+
+func TestNewAESKeys(t *testing.T) {
+	fmt.Println("Key:", NewAESKey())
+}
