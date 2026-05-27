c76b38a649c4548c869fb8d22b2af02ae754544b
youhei <ping.yang@wengold.net>
2026-05-27 19:20:33 +0800

    update xtask spec command

--- utils/xtask/toolbox_task.go
+++ utils/xtask/toolbox_task.go
@@ -21,7 +21,44 @@ import (
 /* For Toolbox Time Task                                               */
 /* ------------------------------------------------------------------- */
 
-// Task datas for multipe generate
+// Task datas for multipe generate.
+//
+// The spec six columns mean ( * * * * * *)：
+//
+//	| second | minture | hour | day  | month | week                 |
+//	| 0-59   | 0-59    | 1-23 | 1-31 | 1-12  | 0-6 (0 means Sunday) |
+//
+// Cron signals：
+//
+//	`*`  : any time
+//	`,`  : separate signal
+//	`-`  : duration
+//	`/n` : do as n times of time duration
+//
+// Cron spec samples:
+//
+// ---
+//
+//	0/30 * * * * *                  every 30s
+//	0 43 21 * * *                   21:43
+//	0 15 05 * * *                   05:15
+//	0 0 17 * * *                    17:00
+//	0 0 17 * * 1                    17:00 in every Monday
+//	0 0,10 17 * * 0,2,3             17:00 and 17:10 in every Sunday, Tuesday and Wednesday
+//	0 0-10 17 1 * *                 17:00 to 17:10 in 1 min duration each time on the first day of month
+//	0 0 0 1,15 * 1                  0:00 on the 1st day and 15th day of month
+//	0 42 4 1 * *                    4:42 on the 1st day of month
+//	0 0 21 * * 1-6                  21:00 from Monday to Saturday
+//	0 0,10,20,30,40,50 * * * *      every 10 min duration
+//	0 */10 * * * *                  every 10 min duration
+//	0 * 1 * * *                     1:00 to 1:59 in 1 min duration each time
+//	0 0 1 * * *                     1:00
+//	0 0 */1 * * *                   0 min of hour in 1 hour duration
+//	0 0 * * * *                     0 min of hour in 1 hour duration
+//	0 2 8-20/3 * * *                8:02, 11:02, 14:02, 17:02, 20:02
+//	0 30 5 1,15 * *                 5:30 on the 1st day and 15th day of month
+//
+// ---
 type WTask struct {
 	Name    string           // monitor task name
 	Func    toolbox.TaskFunc // monitor task execute function
