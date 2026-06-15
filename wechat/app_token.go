23588e67792f0779918c2a23be713b89f70dd918
youhei <ping.yang@wengold.net>
2026-06-15 13:55:34 +0800

    update

--- wechat/app_token.go
+++ wechat/app_token.go
@@ -29,16 +29,37 @@ func (w *WxIFAgent) ToWxAppTokenUrl(requestcode string) string {
 	return strings.Replace(tokenurl, "CODE", requestcode, -1)
 }
 
-// Bind target app id and secret and return request url to get global api access token.
+/* ------------------------------------------------------------------- */
+/* Global API Access Token                                             */
+/* ------------------------------------------------------------------- */
+
+// Return API access token request url of target wechat app, this api maybe not stable,
+// use the utils.GetApiTokenUrl() instead it.
 //
 // see more links
 //
 // - [getAccessToke](https://developers.weixin.qq.com/miniprogram/dev/server/API/mp-access-token/api_getaccesstoken.html)
 // - [Access Token Usage](https://developers.weixin.qq.com/doc/oplatform/developers/dev/AccessToken.html)
-func (w *WxIFAgent) ToWxAccessTokenUrl() string {
+func (w *WxIFAgent) GetAccessTokenUrl() string {
 	tokenurl := wxauth2ApisUrlDomain +
-		"cgi-bin/token?appid=AppID&secret=AppSecret&grant_type=client_credential"
+		"/cgi-bin/token?appid=AppID&secret=AppSecret&grant_type=client_credential"
 	tokenurl = strings.Replace(tokenurl, "APPID", w.AppID, -1)
 	tokenurl = strings.Replace(tokenurl, "SECRET", w.AppSecret, -1)
 	return tokenurl
+}
+
+// Return API access token request url of target wechat app,
+// the wechat app id and secret params input as POST payload.
+//
+//	{
+//	    "grant_type": "client_credential",
+//	    "appid": "APPID",
+//	    "secret": "APPSECRET"
+//	}
+//
+// see more links
+//
+// - [getStableAccessToken](https://developers.weixin.qq.com/miniprogram/dev/server/API/mp-access-token/api_getstableaccesstoken.html)
+func (w *WxIFAgent) GetApiTokenUrl() string {
+	return wxauth2ApisUrlDomain + "/cgi-bin/stable_token"
 }
